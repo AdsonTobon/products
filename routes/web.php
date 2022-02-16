b24f1900fd56849a863f73d403666637547fdc13
Anderson Tobon Perez <anderson.tobonp@gmail.com>
2022-02-15 23:15:24 -0500

    create Controllers and Views for principal methods project products

--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Routes sin controller
-Route::get('/products', function () {
-    return 'This is the list products';
-});
-
-Route::get('/products/create', function () {
-    return 'This is the form to create a products';
-});
-
-Route::post('/products/create', function () {
-    // return 'This is the form to create a products';
-});
-
-Route::get('/products/{product}', function ($product) {
-    return "Showing product with id {$product}";
-});
-
-Route::get('/products/{product}/edit', function ($product) {
-    return "Showing the form to edit the product with id {$product}";
-});
-
-Route::match(['put', 'patch'],'/products/{product}', function ($product) {
-    //
-});
-
-Route::delete('/products/{product}', function ($product) {
-   //
-});
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']); // create
+Route::post('/products', [ProductController::class, 'store']); // store
+Route::get('/products/{product}', [ProductController::class, 'show']); // show
+Route::get('/products/{product}/edit', [ProductController::class, 'edit']); // edit
+Route::match(['put', 'patch'], '/products/{product}', [ProductController::class, 'update']); // update
+Route::delete('/products/{product}', [ProductController::class, 'destroy']); // destroy
